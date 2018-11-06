9a45188296aaaa61db844b70f1fd75ea78cd11c2
Olli Lappalainen <olli@lamia.fi>
2018-11-06 08:42:55 +0200

    Remove unnecessary code

--- App.js
+++ App.js
@@ -17,24 +17,6 @@ const RootStack = createStackNavigator(
 );
 
 export default class App extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			fontLoaded: false,
-		};
-	}
-
-	async componentDidMount() {
-		await Font.loadAsync({
-			'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
-			'roboto-light': require('./assets/fonts/Roboto-Light.ttf'),
-			'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-		});
-
-		this.setState({ fontLoaded: true });
-	}
-
 	render() {
 		return (
 			<RootStack />
